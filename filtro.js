f6c51b411ccb3debd66e37add8c090152259a87e
Emanuel Saucedo <emanuelsaucedo52@gmail.com>
2023-03-28 22:01:24 -0600

    se corrigieron errores del repositorio

--- filtro.js
+++ filtro.js
@@ -7,36 +7,41 @@ const productos = [
   {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
+//Se cambio la manipulacion del dom para que encontrara el elemento por ID
+const li = document.getElementById("lista-de-productos");
+//Se cambio el nombre de la variable para ser por leerlo mejor y se corrijo el querySelector
+const input = document.getElementById('input');
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
-
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
-
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+//Se creo la funcion displayProductos que recibe los datos de productos y ejecuta el ciclo for para mostrarlos
+function displayProductos(productos){
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
   
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
-
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+displayProductos(productos);
+//Se cambio el querySelector por un getElementById para encotrar el boton más facilmente
+const botonDeFiltro = document.getElementById("button");
 
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
-
-  const texto = $i.value;
+//Se cambio la varialbe $i por input ya que es más entendible a que se refiere
+  const texto = input.value;
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
